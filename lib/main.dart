b4a855e8b02f5ba2d8d08c5ffeee0ead43886ef3
Nathan Oliveira <nathan.oliveira@exa.com.br>
2025-05-25 05:27:15 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
-void main() => runApp(CupertinoApp(
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: [
-        DefaultMaterialLocalizations.delegate,
-        DefaultCupertinoLocalizations.delegate,
-        DefaultWidgetsLocalizations.delegate,
-      ],
-      home: ModularApp(module: AppModule(), child: AppWidget()),
-    ));
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: ModularApp(module: AppModule(), child: AppWidget()),
+  ),
+);
